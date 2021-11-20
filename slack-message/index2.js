de95c4515c5ec548623f0b59a9459ef4323ed5ac
Emilie T <et1421@hotmail.com>
2021-11-20 11:07:40 -0500

    f

--- slack-message/index2.js
+++ slack-message/index2.js
@@ -7,6 +7,10 @@ try {
   const cmd = 'lerna ls --json';
   const result =  execSync(cmd).toString();
   console.log('result', result);
+
+  const cmd2 = 'lerna changed';
+  const result2 =  execSync(cmd2).toString();
+  console.log('result2', result2);
 } catch (error) {
   console.log(`Status Code: ${error.status} with '${error.message}'`);
 }
